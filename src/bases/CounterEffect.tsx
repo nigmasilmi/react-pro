6ec55010e9594078c272613a8ae9ca2825bcaf4c
Nigma Silmi <sn.silmi@gmail.com>
2022-08-16 17:54:39 -0400

    greenSock animations and timelines, useRefs with TS

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,4 +1,5 @@
-import { Fragment, useState, useEffect } from "react";
+import { Fragment, useState, useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 interface ICounterProps {
   initialValue: number;
@@ -8,6 +9,7 @@ interface ICounterProps {
 const MAXIMUM_COUNT = 10;
 
 const CounterEffect = ({ initialValue }: ICounterProps) => {
+  const counterRef = useRef<HTMLHeadingElement>(null);
   const [counter, setCounter] = useState<number>(initialValue);
   const handleClick = () => {
     setCounter((prevState) => Math.min(prevState + 1, MAXIMUM_COUNT));
@@ -21,13 +23,27 @@ const CounterEffect = ({ initialValue }: ICounterProps) => {
         "%cMaximum value reached",
         "color: pink; background-color: black;"
       );
+
+      const timeLine = gsap.timeline();
+
+      timeLine
+        .to(counterRef.current, {
+          y: -10,
+          duration: 0.2,
+          ease: "ease.out",
+        })
+        .to(counterRef.current, {
+          y: 0,
+          duration: 1,
+          ease: "bounce.out",
+        });
     }
   }, [counter]);
 
   return (
     <Fragment>
       <h1>CounterEffect</h1>
-      <h2>{counter}</h2>
+      <h2 ref={counterRef}>{counter}</h2>
       <button onClick={handleClick}>+1</button>
     </Fragment>
   );
