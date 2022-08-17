1dca36fbd4e9ef498d6613cfc2b83935fdd81c29
Nigma Silmi <sn.silmi@gmail.com>
2022-08-17 09:20:07 -0400

    Parameterizing custom hook

--- src/bases/CounterHook.tsx
+++ src/bases/CounterHook.tsx
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 import { useCounter } from "../hooks/useCounter";
 
 const CounterHook = () => {
-  const { counter, counterRef, handleClick } = useCounter();
+  const { counter, elementToAnimate, handleClick } = useCounter({
+    maxCount: 5,
+  });
 
   return (
     <Fragment>
       <h1>CounterHook</h1>
-      <h2 ref={counterRef}>{counter}</h2>
+      <h2 ref={elementToAnimate}>{counter}</h2>
       <button onClick={handleClick}>+1</button>
     </Fragment>
   );
--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,38 +1,36 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useLayoutEffect } from "react";
 import { gsap } from "gsap";
 
 const MAXIMUM_COUNT = 10;
 
-export const useCounter = () => {
-  const counterRef = useRef<HTMLHeadingElement>(null);
+export const useCounter = ({ maxCount = 1 }) => {
+  const elementToAnimate = useRef<any>(null);
   const [counter, setCounter] = useState<number>(1);
+  const timeline = useRef(gsap.timeline());
   const handleClick = () => {
     setCounter((prevState) => Math.min(prevState + 1, MAXIMUM_COUNT));
   };
 
+  useLayoutEffect(() => {
+    timeline.current
+      .to(elementToAnimate.current, {
+        y: -10,
+        duration: 0.2,
+        ease: "ease.out",
+      })
+      .to(elementToAnimate.current, {
+        y: 0,
+        duration: 1,
+        ease: "bounce.out",
+      })
+      .pause();
+
+    return () => {};
+  }, []);
+
   useEffect(() => {
-    if (counter < MAXIMUM_COUNT) {
-      return;
-    } else {
-      console.log(
-        "%cMaximum value reached",
-        "color: pink; background-color: black;"
-      );
-
-      const timeLine = gsap.timeline();
-
-      timeLine
-        .to(counterRef.current, {
-          y: -10,
-          duration: 0.2,
-          ease: "ease.out",
-        })
-        .to(counterRef.current, {
-          y: 0,
-          duration: 1,
-          ease: "bounce.out",
-        });
-    }
-  }, [counter]);
-  return { counter, handleClick, counterRef };
+    // if (counter < maxCount) return;
+    timeline.current.play(0);
+  }, [counter, maxCount]);
+  return { counter, handleClick, elementToAnimate };
 };
